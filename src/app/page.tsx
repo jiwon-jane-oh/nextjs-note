0d49ffddc8722b5ccd0d9e31299844bde23f2750
jiwon-jane-oh <cpfldk93@gmail.com>
2024-09-14 18:02:12 -0700

    Font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+
 import styles from './page.module.css'
 import {notFound} from 'next/navigation'
 import os from 'os' //노드 APIs
 import { useState } from 'react'
 import Counter from '@/components/Counter'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   console.log('hello-server')
